d7b60417069cbdd6becbf2a06c7ab46d628f3878
Amir etemadkhelejani <etemadcloud@gmail.com>
2022-12-24 13:26:53 +0100

    Fix diff query to return correct result

--- pkg/repository/follower.go
+++ pkg/repository/follower.go
@@ -18,16 +18,18 @@ func (u *FollowerRepository) SaveList(follower []model.Follower) (bool, error) {
 	return true, nil
 }
 
-func (u *FollowerRepository) GetDiff(tracker model.Tracker) ([]model.Follower, []model.Follower) {
+func (u *FollowerRepository) GetDiff(username string, tracker model.Tracker) ([]model.Follower, []model.Follower) {
 	var followers []model.Follower
 	var unfollows []model.Follower
 
 	u.DB.
-		Raw("select * from followers where track_id = ? and twitter_id not in (select twitter_id from followers where track_id <> ?)", tracker.ID, tracker.ID).
+		Raw("SELECT * FROM followers f INNER JOIN trackers t ON f.track_id = t.id and t.source_value = ? and t.source = ? where f.track_id = ? and f.twitter_id not in (SELECT twitter_id FROM followers where track_id IN (select id from trackers where source_value = ? and source = ? and id < ?))",
+			username, model.TrackerFetchFollower, tracker.ID, username, model.TrackerFetchFollower, tracker.ID).
 		Scan(&followers)
 
 	u.DB.
-		Raw("select * from followers where track_id <> ? and twitter_id not in (select twitter_id from followers where track_id = ?)", tracker.ID, tracker.ID).
+		Raw("SELECT * FROM followers f INNER JOIN trackers t ON f.track_id = t.id and t.source_value = ? and t.source = ? where f.track_id < ? and f.twitter_id not in (SELECT twitter_id FROM followers where track_id IN (select id from trackers where source_value = ? and source = ? and id = ?))",
+			username, model.TrackerFetchFollower, tracker.ID, username, model.TrackerFetchFollower, tracker.ID).
 		Scan(&unfollows)
 
 	return followers, unfollows
